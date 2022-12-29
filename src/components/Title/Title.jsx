ca6b1da2b3361b23610431d4dfbe0b9973b702d0
alexehouston <alexehouston@gmail.com>
2022-12-29 17:16:17 -0600

    adjust spacing

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,7 +3,7 @@ import './Title.css';
 export default function Title() {
     return (
         <div className="Title">
-            <h1>elia gal</h1>
+            <h1>elia gal</h1><br />
             <h6>hair stylist</h6>
         </div>
     );
