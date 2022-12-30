9bcdf39bdf8ec16aea871df54f16c0732b3b15b1
alexehouston <alexehouston@gmail.com>
2022-12-29 23:53:51 -0600

    update css

--- src/pages/NBR/NBR.jsx
+++ src/pages/NBR/NBR.jsx
@@ -2,21 +2,23 @@ import './NBR.css';
 
 export default function NBR() {
     return (
-        <div className="NBR">
-            <div className="nbr-info">
-                <h1>nbr extensions</h1>
-            </div>
-            <div className="nbr-questions">
-                <h4>What are Natural Beaded Row Hair Extensions?</h4>
-                <p>Natural Beaded Row Hair Extensions (aka NBR) is a method of hair extensions that uses hand-tied wefts of hair to create length and volume. They are installed by applying a track using beads and string which helps to evenly distribute the weight of the extension hair across the scalp. The NBR Extension method was created to achieve maximum results with little to no damage or discomfort! Wearing NBR Extensions does not limit the ways you can wash, dry, and style your hair the way other extension methods do. You can swim, work out, wear your hair up, and go about your day to day life almost exactly the way you would if you weren't wearing extensions!</p>
-                <h4>Will They Show?</h4>
-                <p>NBR Extensions are custom colored and cut to create a seamless blend based on the unique needs of your hair. They are placed and installed in a way that allows them to hide seamlessly in the hair. You can wear them up, down, straight or curled without having to worry about them showing. NBR Hair Extensions are available in a variety of colors and textures, and the color is further customized prior to your install appointment to ensure that they mirror your hair perfectly.</p>
-                <h4>Are NBR Hair Extensions high maintenance?</h4>
-                <p>NBR Hair Extensions need to be moved up every 6-8 weeks depending on your hair type. Finer hair textures can expect to be closer to the 6 week mark, while coarser hair textures can expect to be closer to the 8 week mark. At each maintenance appointment your NBR Extensions are removed and your natural hair is custom colored. Your extension hair is custom colored to match your natural hair and then they are re-installed! You can expect to spend an average of 5-6 hours in the salon on the day of your install appointment, and an average of 3-4 hours for each maintenance appointment. There can be a bit of a learning curve involved when you first start wearing NBR Extensions, but after you get used to washing + drying + styling them they simplify your day to day routine significantly! NBR Extensions hold a style much longer than natural hair does, and they don’t get dirty as quickly. This allows you to go longer in between washes, and to get by with minimal style touch-ups in between your wash days.</p>
-                <h4>Will they damage my hair?</h4>
-                <p>When installed correctly and cared for properly NBR Extensions do not cause damage to your hair. In fact, the majority of NBR-wearers see an increase in the strength and length of their hair during their time wearing Natural Beaded Rows! There is no tape or adhesive used in the NBR method, which makes removal quick, easy, and painless at each maintenance appointment.</p>
+        <>
+            <div className="NBR">
+                <div className="nbr-info">
+                    <h1>nbr extensions</h1>
+                </div>
+                <div className="nbr-questions">
+                    <h4>What are Natural Beaded Row Hair Extensions?</h4>
+                    <p>Natural Beaded Row Hair Extensions (aka NBR) is a method of hair extensions that uses hand-tied wefts of hair to create length and volume. They are installed by applying a track using beads and string which helps to evenly distribute the weight of the extension hair across the scalp. The NBR Extension method was created to achieve maximum results with little to no damage or discomfort! Wearing NBR Extensions does not limit the ways you can wash, dry, and style your hair the way other extension methods do. You can swim, work out, wear your hair up, and go about your day to day life almost exactly the way you would if you weren't wearing extensions!</p>
+                    <h4>Will They Show?</h4>
+                    <p>NBR Extensions are custom colored and cut to create a seamless blend based on the unique needs of your hair. They are placed and installed in a way that allows them to hide seamlessly in the hair. You can wear them up, down, straight or curled without having to worry about them showing. NBR Hair Extensions are available in a variety of colors and textures, and the color is further customized prior to your install appointment to ensure that they mirror your hair perfectly.</p>
+                    <h4>Are NBR Hair Extensions high maintenance?</h4>
+                    <p>NBR Hair Extensions need to be moved up every 6-8 weeks depending on your hair type. Finer hair textures can expect to be closer to the 6 week mark, while coarser hair textures can expect to be closer to the 8 week mark. At each maintenance appointment your NBR Extensions are removed and your natural hair is custom colored. Your extension hair is custom colored to match your natural hair and then they are re-installed! You can expect to spend an average of 5-6 hours in the salon on the day of your install appointment, and an average of 3-4 hours for each maintenance appointment. There can be a bit of a learning curve involved when you first start wearing NBR Extensions, but after you get used to washing + drying + styling them they simplify your day to day routine significantly! NBR Extensions hold a style much longer than natural hair does, and they don’t get dirty as quickly. This allows you to go longer in between washes, and to get by with minimal style touch-ups in between your wash days.</p>
+                    <h4>Will they damage my hair?</h4>
+                    <p>When installed correctly and cared for properly NBR Extensions do not cause damage to your hair. In fact, the majority of NBR-wearers see an increase in the strength and length of their hair during their time wearing Natural Beaded Rows! There is no tape or adhesive used in the NBR method, which makes removal quick, easy, and painless at each maintenance appointment.</p>
+                </div>
             </div>
             <p className="scroll">scroll for all FAQ</p>
-        </div>
+        </>
     );
 }
