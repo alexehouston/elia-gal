fd552ede767ee0b71c507c8571c70c2d27559907
ahouston <alexehouston@gmail.com>
2023-05-24 13:35:45 -0500

    update links

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -5,13 +5,16 @@ export default function Contact() {
         <div className="Contact">
             <div className="contact-info">
                 <h1>contact elia</h1><br/>
-                <a href="https://www.instagram.com/eliarene/" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
+                <a href="https://www.instagram.com/eliarenegal/" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
                 &nbsp;&nbsp;&nbsp;
-                <a href="mailto:galxbeauty@gmail.com" target="_blank" rel="noreferrer"><i className="fa-regular fa-envelope"></i></a>
+                <a href="mailto:galxbeauty@gmail.com" target="_blank" rel="noreferrer"><i className="fa-solid fa-envelope"></i></a>
                 <div className="salon">
                     <h2>KASHMIR</h2>
-                    <li className="hover"><a href="https://www.kashmirextensions.com/" target="_blank" rel="noreferrer">kashmirextensions.com</a></li>
-                    <li className="hover"><a href="tel:310-340-1053">(310) 340-1053</a></li>
+                    <div className="contact-icons">
+                        <li><a href="https://www.kashmirextensions.com/" target="_blank" rel="noreferrer"><i className="fa-solid fa-globe"></i></a></li>
+                        <li id="kashmir-ig"><a href="https://www.instagram.com/kashmirextensions/" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a></li>
+                        <li><a href="tel:310-340-1053"><i className="fa-solid fa-phone"></i></a></li>
+                    </div>
                     <li className="hover"><a href="https://www.google.com/maps/place/NBR+Hair+Extensions+by+Kashmir/@34.0975552,-118.3677816,15z/data=!4m2!3m1!1s0x0:0x6d79f74f303ec2bb?sa=X&ved=2ahUKEwjq6omii-H8AhXEkmoFHVRpCi0Q_BJ6BAhkEAg" target="_blank" rel="noreferrer">
                         1481 Havenhurst Dr<br/>
                         West Hollywood, CA 90046
