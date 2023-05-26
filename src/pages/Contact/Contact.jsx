771dea29e159a98bc12294f6ccf92d71402735d2
ahouston <alexehouston@gmail.com>
2023-05-26 09:40:01 -0500

    update links

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -14,6 +14,7 @@ export default function Contact() {
                         <li><a href="https://www.kashmirextensions.com/" target="_blank" rel="noreferrer"><i className="fa-solid fa-globe"></i></a></li>
                         <li id="kashmir-ig"><a href="https://www.instagram.com/kashmirextensions/" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a></li>
                         <li><a href="sms:3103401053"><i className="fa-solid fa-comment"></i></a></li>
+                        <li><a href="mailto:info@kashmirextensions.com" target="_blank" rel="noreferrer"><i className="fa-solid fa-envelope"></i></a></li>
                     </div>
                     <li className="hover"><a href="https://www.google.com/maps/place/NBR+Hair+Extensions+by+Kashmir/@34.0975552,-118.3677816,15z/data=!4m2!3m1!1s0x0:0x6d79f74f303ec2bb?sa=X&ved=2ahUKEwjq6omii-H8AhXEkmoFHVRpCi0Q_BJ6BAhkEAg" target="_blank" rel="noreferrer">
                         1481 Havenhurst Dr<br/>
