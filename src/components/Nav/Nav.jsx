f0a8025c03de66d23950e111ebd1e0d8249b9f44
ahouston <alexehouston@gmail.com>
2023-07-12 18:13:40 -0500

    update css

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,22 +3,22 @@ import './Nav.css';
 export default function Nav({ currentPage, setCurrentPage }) {
     return (
         <div className="Nav">
-            <ul>
-                {currentPage === 'home' ? (
-                    <>
+            {currentPage === 'home' ? (
+                <ul class="animate__animated animate__fadeInDown animate__slow">
+                    <li className="hover" onClick={() => setCurrentPage('services')}>services</li>
+                    <li className="hover" onClick={() => setCurrentPage('about')}>about</li>
+                    <li className="hover" onClick={() => setCurrentPage('contact')}>contact</li>
+                </ul>
+            ) : (
+                <>
+                    <h1 className="hover animate__animated animate__fadeInLeft animate__slow" id="elia" onClick={() => setCurrentPage('home')}>elia gal</h1>
+                    <ul>
                         <li className="hover" onClick={() => setCurrentPage('services')}>services</li>
                         <li className="hover" onClick={() => setCurrentPage('about')}>about</li>
                         <li className="hover" onClick={() => setCurrentPage('contact')}>contact</li>
-                    </>
-                ) : (
-                    <>
-                        <h1 className="hover" id="elia" onClick={() => setCurrentPage('home')}>elia gal</h1>
-                        <li className="hover" onClick={() => setCurrentPage('services')}>services</li>
-                        <li className="hover" onClick={() => setCurrentPage('about')}>about</li>
-                        <li className="hover" onClick={() => setCurrentPage('contact')}>contact</li>
-                    </>
-                )}
-            </ul>
-        </div>
+                    </ul>
+                </>
+            )}
+    </div>
     );
 }
